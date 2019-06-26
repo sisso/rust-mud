e76f0d2052020cf44be642c14a33fed4660f7f0f
sisso <sisso@sisso.com.br>
2019-06-26 20:17:04 +0200

    clean up commands

--- src/game/actions.rs
+++ src/game/actions.rs
@@ -2,95 +2,58 @@ use super::comm;
 use super::domain::*;
 use super::controller::Output;
 
-pub enum Command {
-    Move {
-        player_id: PlayerId,
-        dir: Dir
-    },
-    Say {
-        player_id: PlayerId,
-        msg: String
-    }
-}
-
-impl Command {
-    pub fn get_player_id(&self) -> &PlayerId {
-        match self {
-            Command::Move { player_id, ..} => player_id,
-            Command::Say { player_id, ..} => player_id,
-        }
-    }
-}
-
-pub fn look(game: &mut Container, outputs: &mut Vec<Output>, player_id: &PlayerId) {
-    let ctx = game.get_player_context(&player_id);
+pub fn look(container: &mut Container, outputs: &mut Vec<Output>, player_id: &PlayerId) {
+    let ctx = container.get_player_context(&player_id);
 
     outputs.push(Output::Private {
         player_id: player_id.clone(),
-        msg: comm::get_look_description(game, &ctx)
+        msg: comm::get_look_description(container, &ctx)
     })
 }
 
-pub fn say(game: &mut Container, outputs: &mut Vec<Output>, player_id: &PlayerId, msg: String) {
-    handle(game, outputs, Command::Say {
-        player_id: player_id.clone(),
-        msg: msg
-    });
+pub fn say(container: &mut Container, outputs: &mut Vec<Output>, player_id: &PlayerId, msg: String) {
+    let ctx = container.get_player_context(player_id);
+    let player_msg = format!("you say '{}'\n", msg);
+    let room_msg = format!("{} says '{}'\n", ctx.avatar.label, msg);
+
+    outputs.push(Output::private(player_id.clone(), player_msg));
+    outputs.push(Output::room(player_id.clone(), ctx.avatar.room_id, room_msg));
 }
 
-pub fn mv(game: &mut Container, outputs: &mut Vec<Output>, player_id: &PlayerId, dir: Dir) {
-    handle(game, outputs, Command::Move {
-        player_id: player_id.clone(),
-        dir: dir
-    });
-}
+pub fn mv(container: &mut Container, outputs: &mut Vec<Output>, player_id: &PlayerId, dir: Dir) {
+    let ctx = container.get_player_context(player_id);
+    let player_id = player_id.clone();
 
+    let exit_room_id = ctx.room
+        .exits
+        .iter()
+        .find(|e| e.0 == dir)
+        .map(|i| i.1);
 
-pub fn handle(game: &mut Container, outputs: &mut Vec<Output>, command: Command) {
-    let player_id = command.get_player_id().clone();
-    let ctx = game.get_player_context(&player_id);
+    match exit_room_id {
+        Some(exit_room_id) => {
+            let previous_room_id = ctx.avatar.room_id;
 
-    match command {
-        Command::Move { player_id, dir } => {
-            let exit_room_id = ctx.room
-                .exits
-                .iter()
-                .find(|e| e.0 == dir)
-                .map(|i| i.1);
+            // change entity in place
+            let mut mob = ctx.avatar.clone();
+            mob.room_id = exit_room_id;
+            container.update_mob(mob);
 
-            match exit_room_id {
-                Some(exit_room_id) => {
-                    let previous_room_id = ctx.avatar.room_id;
+            // get new player ctx
+            let ctx = container.get_player_context(&player_id);
 
-                    // change entity in place
-                    let mut mob = ctx.avatar.clone();
-                    mob.room_id = exit_room_id;
-                    game.update_mob(mob);
+            let look = comm::get_look_description(&container, &ctx);
 
-                    // get new player ctx
-                    let ctx = game.get_player_context(&player_id);
+            let player_msg = format!("you move to {}!\n\n{}", dir, look);
+            let enter_room_msg = format!("{} comes from {}.\n", ctx.avatar.label, dir.inv());
+            let exit_room_msg = format!("{} goes to {}.\n", ctx.avatar.label, dir);
 
-                    let look = comm::get_look_description(&game, &ctx);
-
-                    let player_msg = format!("you move to {}!\n\n{}", dir, look);
-                    let enter_room_msg = format!("{} comes from {}.\n", ctx.avatar.label, dir.inv());
-                    let exit_room_msg = format!("{} goes to {}.\n", ctx.avatar.label, dir);
-
-                    outputs.push(Output::private(player_id, player_msg));
-                    outputs.push(Output::room(player_id, previous_room_id, exit_room_msg));
-                    outputs.push(Output::room(player_id, ctx.room.id, enter_room_msg));
-                },
-                None => {
-                    outputs.push(Output::private(player_id, format!("not possible to move to {}!\n", dir)));
-                }
-            }
+            outputs.push(Output::private(player_id, player_msg));
+            outputs.push(Output::room(player_id, previous_room_id, exit_room_msg));
+            outputs.push(Output::room(player_id, ctx.room.id, enter_room_msg));
         },
-        Command::Say { player_id, msg } => {
-            let player_msg = format!("you say '{}'\n", msg);
-            let room_msg = format!("{} says '{}'\n", ctx.avatar.label, msg);
-
-            outputs.push(Output::private(player_id.clone(), player_msg));
-            outputs.push(Output::room(player_id.clone(), ctx.avatar.room_id, room_msg));
-        },
+        None => {
+            outputs.push(Output::private(player_id, format!("not possible to move to {}!\n", dir)));
+        }
     }
 }
