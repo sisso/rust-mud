b5fbe084c5e7e7e536f31cf90ad3b4688f437748
sisso <sisso@sisso.com.br>
2019-12-30 20:20:12 +0100

    Prototyping Trigger and Timer

---
+++ commons/tests/timer_test.rs
@@ -0,0 +1,98 @@
+use std::collections::VecDeque;
+
+struct Entry<T> {
+    time: f64,
+    value: T,
+}
+
+struct Timer<T> {
+    current: f64,
+    events: Vec<Entry<T>>,
+}
+
+impl <T> Timer<T> {
+    pub fn new() -> Self {
+        Timer { 
+            current: 0.0, 
+            events: Default::default(),
+         }
+    }
+
+    pub fn schedule(&mut self, value: T, time: f64) {
+        self.events.push(Entry { time, value: value } );
+    }
+
+    pub fn check(&mut self, total_time: f64) -> Vec<T> {
+        assert!(self.current <= total_time);
+
+        self.current = total_time;
+
+        let mut indexes = VecDeque::new();
+        for (i, e) in self.events.iter().enumerate() {
+            if e.time <= total_time {
+                indexes.push_front(i);
+            }
+        }
+
+        let mut buffer = VecDeque::new();
+        for i in indexes {
+            let e = self.events.remove(i);
+            buffer.push_front(e.value);
+        }
+
+        buffer.into()
+   }
+}
+
+#[test]
+pub fn test_timer() {
+    let mut timer: Timer<&str> = Timer::new();
+    
+    timer.schedule("Z", 0.0);
+    timer.schedule("A", 2.0);
+    timer.schedule("B", 3.5);
+    timer.schedule("C", 4.0);
+    timer.schedule("D", 10.0);
+
+    // initial element
+    let mut result = timer.check(0.0);
+    assert_eq!(result.len(), 1);
+    assert_eq!("Z", result.remove(0));
+
+    // no new element
+    let mut result = timer.check(1.0);
+    assert_eq!(result.len(), 0);
+
+    // normal case
+    let mut result = timer.check(2.01);
+    assert_eq!(result.len(), 1);
+    assert_eq!("A", result.remove(0));
+
+    // multiple case
+    let mut result = timer.check(4.00);
+    assert_eq!(result.len(), 2);
+    assert_eq!("B", result.remove(0));
+    assert_eq!("C", result.remove(0));
+
+    // no new element
+    let mut result = timer.check(6.0);
+    assert_eq!(result.len(), 0);
+
+    // huge jump
+    let mut result = timer.check(1000.00);
+    assert_eq!(result.len(), 1);
+    assert_eq!("D", result.remove(0));
+
+    // no new element
+    let mut result = timer.check(10000.0);
+    assert_eq!(result.len(), 0);
+}
+
+#[test]
+#[should_panic]
+pub fn test_timer_fail_if_reverse_time() {
+    let mut timer: Timer<u32> = Timer::new();
+    timer.check(3.0);
+    timer.check(1.0);
+}
+
---
+++ commons/tests/trigger_test.rs
@@ -0,0 +1,175 @@
+use std::collections::{VecDeque, HashMap};
+
+pub struct Listener {
+    id: u32,
+    kind: u32,
+}
+
+pub struct Trigger<T> {
+    next_listener: u32,
+    /// index is even type, key is listener id, value is position in events queue
+    listeners_per_kind: HashMap<u32, HashMap<u32, usize>>,
+    /// index is event type, value is published events
+    events: HashMap<u32, Vec<T>>,
+}
+
+impl <T> Trigger<T> {
+    pub fn new() -> Self {
+        Trigger {
+            next_listener: 0,
+            listeners_per_kind: HashMap::new(),
+            events: HashMap::new(),
+        }
+    }
+
+    pub fn register(&mut self, event_kind: u32) -> Listener {
+        let next = self.next_listener;
+        self.next_listener += 1;
+
+        let mut listeners = self.listeners_per_kind.entry(event_kind)
+            .or_default();
+
+        listeners.insert(next, 0);
+
+        Listener {
+            id: next,
+            kind: event_kind,
+        }
+    }
+
+    pub fn push(&mut self, event_kind: u32, event: T) {
+        let mut events = self.events.entry(event_kind)
+            .or_default();
+
+        events.push(event);
+    }
+
+    pub fn take(&mut self, listener: &Listener) -> Vec<&T> {
+        let index = self.listeners_per_kind.get_mut(&listener.kind)
+            .unwrap()
+            .get_mut(&listener.id)
+            .unwrap();
+
+        match self.events.get(&listener.kind) {
+            Some(vec) => {
+                let current = *index;
+                *index = vec.len();
+                vec.iter().skip(current).collect()
+            },
+            None => Vec::new(),
+        }
+    }
+
+    pub fn len(&self, event_kind: u32) -> usize {
+        self.events.get(&event_kind)
+            .map(|vec| vec.len())
+            .unwrap_or(0)
+    }
+
+    pub fn gc(&mut self) {
+        let min_index_per_kind = self.listeners_per_kind.iter()
+            .map(|(&kind, listeners)| {
+                let min = listeners.iter()
+                    .map(|(_, &pos)| pos)
+                    .min()
+                    .unwrap_or(0);
+
+                (kind, min)
+            }).collect::<Vec<_>>();
+
+        for (kind, min_index) in min_index_per_kind {
+            // update events
+            let events = self.events.get_mut(&kind).unwrap();
+            // TODO: serious? can not make it worse?
+            for _ in 0..min_index {
+                events.remove(0);
+            }
+
+            // update indexes
+            self.listeners_per_kind.get_mut(&kind)
+                .unwrap()
+                .iter_mut()
+                .for_each(|(_, pos)| {
+                    *pos -= min_index;
+                })
+        }
+    }
+}
+
+#[test]
+fn test_listeners() {
+    let mut trigger = Trigger::new();
+
+    let listener_0 = trigger.register(0);
+    let listener_1 = trigger.register(0);
+    let listener_2 = trigger.register(1);
+
+    trigger.push(0, 0);
+    trigger.push(0, 1);
+
+    // take first events
+    let result = trigger.take(&listener_0);
+    assert_eq!(2, result.len());
+    assert_eq!(0, *result[0]);
+    assert_eq!(1, *result[1]);
+
+    let result = trigger.take(&listener_1);
+    assert_eq!(2, result.len());
+    assert_eq!(0, *result[0]);
+    assert_eq!(1, *result[1]);
+
+    let result = trigger.take(&listener_2);
+    assert_eq!(0, result.len());
+
+    // second time is empty
+    let result = trigger.take(&listener_0);
+    assert_eq!(0, result.len());
+
+    // noch einmal
+    trigger.push(1, 2);
+
+    let result = trigger.take(&listener_0);
+    assert_eq!(0, result.len());
+
+    let result = trigger.take(&listener_1);
+    assert_eq!(0, result.len());
+
+    let result = trigger.take(&listener_2);
+    assert_eq!(1, result.len());
+    assert_eq!(2, *result[0]);
+}
+
+#[test]
+pub fn test_events_garbage_collect() {
+    let mut trigger = Trigger::new();
+
+    let listener_0 = trigger.register(0);
+    let listener_1 = trigger.register(0);
+
+    for i in 0..100 {
+        trigger.push(0, i);
+    }
+
+    let result = trigger.take(&listener_0);
+    assert_eq!(100, result.len());
+
+    let result = trigger.take(&listener_1);
+    assert_eq!(100, result.len());
+
+    for i in 0..10 {
+        trigger.push(0, i);
+    }
+    assert_eq!(110, trigger.len(0));
+
+    trigger.gc();
+    assert_eq!(10, trigger.len(0));
+
+    let result = trigger.take(&listener_0);
+    assert_eq!(10, result.len());
+
+    let result = trigger.take(&listener_1);
+    assert_eq!(10, result.len());
+
+    trigger.gc();
+    assert_eq!(0, trigger.len(0));
+}
