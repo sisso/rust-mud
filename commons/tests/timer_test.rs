913eacd09523f655fe72a1de6f9e66b90cfca5f6
sisso <sisso@sisso.com.br>
2020-01-05 20:40:14 +0100

    WIP: adding peek to timer
    - having a hard time to find some sorted collection

--- commons/tests/timer_test.rs
+++ commons/tests/timer_test.rs
@@ -1,4 +1,5 @@
-use std::collections::VecDeque;
+use std::collections::{LinkedList, VecDeque, BinaryHeap};
+use std::cmp::Reverse;
 
 struct Entry<T> {
     time: f64,
@@ -42,10 +43,14 @@ impl <T> Timer<T> {
 
         buffer.into()
    }
+
+    pub fn peek(&self) -> Option<f64> {
+        unimplemented!()
+    }
 }
 
 #[test]
-pub fn test_timer() {
+fn test_timer() {
     let mut timer: Timer<&str> = Timer::new();
     
     timer.schedule("Z", 0.0);
@@ -90,9 +95,62 @@ pub fn test_timer() {
 
 #[test]
 #[should_panic]
-pub fn test_timer_fail_if_reverse_time() {
+fn test_timer_fail_if_reverse_time() {
     let mut timer: Timer<u32> = Timer::new();
     timer.check(3.0);
     timer.check(1.0);
+}
+
+#[test]
+fn test_timer_peek_should_return_next_trigger() {
+    let mut timer: Timer<&str> = Timer::new();
+    assert!(timer.peek().is_none());
+
+    timer.schedule("A", 2.0);
+    timer.schedule("B", 10.0);
+
+    assert_eq!(timer.peek(), Some(2.0));
+
+    let _ = timer.check(2.5);
+    assert_eq!(timer.peek(), Some(10.0));
+
+    let _ = timer.check(20.0);
+    assert!(timer.peek().is_none());
+}
+
+#[test]
+fn test_new_collection() {
+    let mut list = BinaryHeap::new();
+    list.push(Reverse(total_time_to_cap_time(1.0)));
+    list.push(Reverse(total_time_to_cap_time(5.0)));
+    list.push(Reverse(total_time_to_cap_time(2.0)));
+
+    println!("{:?}", list.peek().map(|i| cap_time_to_total_time(i.0)));
+    println!("{:?}", list.pop().map(|i| cap_time_to_total_time(i.0)));
+
+    panic!();
+}
+
+fn cap_time_to_total_time(cap_time: u64) -> f64 {
+    (cap_time as f64) / 100.0
+}
+
+fn total_time_to_cap_time(total_time: f64) -> u64 {
+    (total_time * 100.0) as u64
+}
+
+#[test]
+fn test_to_cap_time() {
+    assert_eq!(total_time_to_cap_time(0.0), 0);
+    assert_eq!(total_time_to_cap_time(1.1), 110);
+    assert_eq!(total_time_to_cap_time(2.22), 222);
+    assert_eq!(total_time_to_cap_time(3.333), 333);
+    assert_eq!(total_time_to_cap_time(444444.4444), 44444444);
+}
+
+#[test]
+fn test_from_cap_time() {
+    assert_eq!(cap_time_to_total_time(0), 0.0);
+    assert_eq!(cap_time_to_total_time(110), 1.10);
 }
 
